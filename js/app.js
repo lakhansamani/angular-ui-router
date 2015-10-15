3aa6e38c477224f7662b8f47a1e6da85fb7e1c13
lakhansamani <lakhan.m.samani@gmail.com>
2015-10-16 00:39:48 +0530

    Adds more nested routings

--- js/app.js
+++ js/app.js
@@ -5,7 +5,27 @@ app.config(['$stateProvider', '$urlRouterProvider',function($stateProvider,$urlR
 		url: '/',
 		templateUrl: 'templates/main.html',
 		controller: 'MainCtrl'
-	});	
+	})
+	.state('main.about',{
+		abstract:true,
+		views:{
+			'menu':{
+				templateUrl:'templates/menu.html'
+			},
+			'':{
+				templateUrl:'templates/about.html'
+			}
+		},
+	})
+	.state('main.about.vision',{
+		url:'vision',
+		templateUrl:'templates/vision.html'
+	})
+	.state('main.about.director',{
+		url:'director',
+		'templateUrl':'templates/director.html'
+	})
+	;	
     $urlRouterProvider.otherwise('/');
 }]);
 angular.module('myApp.controllers',[]);
